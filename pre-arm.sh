83d41d057414c29115686178b710ece53787e0c9
Audun Solemdal <auls@equinor.com>
2019-12-02 18:49:10 +0100

    pre-arm: update reply urls per environment

--- pre-arm.sh
+++ pre-arm.sh
@@ -3,6 +3,7 @@
 # It also configures the client to run kubectl commands on the cluster (.kube/config)
 # All variables should be defined in a file called ".env"
 set -e
+source .env
 
 function service_principal_exist {
     az ad sp show --id http://$1 --query objectId -o tsv > /dev/null 2>&1
@@ -37,3 +38,38 @@ fi
 
 echo
 echo " If the ARM deployment fails with 'service principal does not exist' run the script again.."
+
+# Get existing reply urls for app registration
+EXISTINGREGS=$(az ad app list --display-name 'SDP Team' --query [0].replyUrls -o tsv)
+
+echo " Creating List of Reply Urls per environment (leave variable for prod cluster blank)..."
+# Make sure there are no spaces at the end of each line!"
+cat << EOF > newapplist.json
+https://alertmanager.${PREFIX}sdpaks.equinor.com/oauth2/callback
+https://aware.${PREFIX}sdpaks.equinor.com/oauth2/callback
+https://gitlab.${PREFIX}sdpaks.equinor.com/users/auth/azure_oauth2/callback
+https://grafana.${PREFIX}sdpaks.equinor.com/login/generic_oauth
+https://kibana.${PREFIX}sdpaks.equinor.com/oauth2/callback
+https://monitor.${PREFIX}sdpaks.equinor.com/oauth2/callback
+https://prometheus.${PREFIX}sdpaks.equinor.com/oauth2/callback
+https://release-aware.${PREFIX}sdpaks.equinor.com/oauth2/callback
+https://sdp-web.${PREFIX}sdpaks.equinor.com/oauth2/callback
+EOF
+
+# Sort and format list to newline format
+echo
+echo $EXISTINGREGS | tr ' ' '\n' | sort | tr '\n' ' ' | tr " " "\n" >  cat > existingregs.json
+
+# Create differential list (requires sorted newline)
+DIFFLIST=$(awk 'NR==FNR{a[$0]=1;next}!a[$0]' existingregs.json newapplist.json)
+# Cleanup files
+rm existingregs.json & rm newapplist.json
+
+# Hardcoded object Id for 'SDP Team' (required)
+# Important, no quotes around the below command! az cli requires space separated values
+echo " Updating existing app registration with the following reply urls:"
+az ad app update --add replyUrls ${DIFFLIST[@]} --id 3b014a2c-797d-43aa-a379-2344fc04b8cc
+
+echo "${DIFFLIST[@]}"
+echo
+echo " Reply urls successfully added."
